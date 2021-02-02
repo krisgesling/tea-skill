0ce81a10b2a5e0b820a6082af63b551b30ac947b
Kris Gesling <kris.gesling@mycroft.ai>
2021-02-02 11:53:22 +0930

    working example

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/coffee.svg" card_color="#22A7F0" width="50" height="50" style="vertical-align:bottom"/> Tea
-Example skill showing conversational context
+Example Skill showing conversational context
 
 ## About
-
+A quick example Skill to match the Tea Skill outlined in the Mycroft documentation.
 
 ## Examples
+- Bring me some tea
 
 ## Credits
 MycroftAI
--- __init__.py
+++ __init__.py
@@ -1,15 +1,52 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft import MycroftSkill, intent_handler
+from mycroft.skills.context import adds_context, removes_context
 
 
-class Tea(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+class TeaSkill(MycroftSkill):
+    @intent_handler(IntentBuilder("TeaIntent").require("bring").require("tea"))
+    @adds_context("MilkContext")
+    def handle_tea_intent(self, message):
+        self.milk = False
+        self.speak("Of course, would you like Milk with that?", expect_response=True)
 
-    @intent_file_handler('tea.intent')
-    def handle_tea(self, message):
-        self.speak_dialog('tea')
+    @intent_handler(
+        IntentBuilder("NoMilkIntent").require("no").require("MilkContext").build()
+    )
+    @removes_context("MilkContext")
+    @adds_context("HoneyContext")
+    def handle_no_milk_intent(self, message):
+        self.speak("all right, any Honey?", expect_response=True)
+
+    @intent_handler(
+        IntentBuilder("YesMilkIntent").require("yes").require("MilkContext").build()
+    )
+    @removes_context("MilkContext")
+    @adds_context("HoneyContext")
+    def handle_yes_milk_intent(self, message):
+        self.milk = True
+        self.speak("What about Honey?", expect_response=True)
+
+    @intent_handler(
+        IntentBuilder("NoHoneyIntent").require("no").require("HoneyContext").build()
+    )
+    @removes_context("HoneyContext")
+    def handle_no_honey_intent(self, message):
+        if self.milk:
+            self.speak("Heres your Tea with a dash of Milk")
+        else:
+            self.speak("Heres your Tea, straight up")
+
+    @intent_handler(
+        IntentBuilder("YesHoneyIntent").require("yes").require("HoneyContext").build()
+    )
+    @removes_context("HoneyContext")
+    def handle_yes_honey_intent(self, message):
+        if self.milk:
+            self.speak("Heres your Tea with Milk and Honey")
+        else:
+            self.speak("Heres your Tea with Honey")
 
 
 def create_skill():
-    return Tea()
-
+    return TeaSkill()
